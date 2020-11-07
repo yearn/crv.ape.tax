570e41ac7f310508eb84fe57557cc4d8b7c69d87
banteg <banteeg@gmail.com>
2020-11-07 08:25:44 +0300

    feat: add vecrv vault

--- truffle-config.js
+++ truffle-config.js
@@ -3,5 +3,9 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "vapp/src/contracts"),
-
+  compilers: {
+    solc: {
+      version: "0.6.12"
+    }
+  }
 };
