115c637d88b10cae92c7f83a3d325c6744bb8da4
banteg <banteeg@gmail.com>
2020-11-07 08:20:45 +0300

    feat: init drizzle vue project

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.7.0;
+
+contract SimpleStorage {
+    event StorageSet(string _message);
+
+    uint public storedData;
+
+    function set(uint x) public {
+        storedData = x;
+
+        emit StorageSet("Data stored successfully!");
+    }
+}
