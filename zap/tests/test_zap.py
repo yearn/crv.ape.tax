b29d0bb0385dad1415eecb7f363c502ef7726e47
listonjesse <listonjesse@yahoo.com>
2020-11-08 00:56:02 -0800

    draft: zap now claims curve on all gauges and the vesting contract

--- zap/tests/test_zap.py
+++ zap/tests/test_zap.py
@@ -1,6 +1,9 @@
-def test_zap(zap, user, crv, vesting, vault):
-    assert vesting.balanceOf(user) > 0
+def test_zap(zap, user, crv, vesting, vault, minter, gauges):
     before = vault.balanceOf(user)
+    assert vesting.balanceOf(user) > 0
+    minter.toggle_approve_mint(zap.address, {"from": user})
     crv.approve(zap, 2 ** 256 - 1, {"from": user})
-    zap.zap({"from": user})
+    zap.zap(gauges, {"from": user})
     assert vault.balanceOf(user) > before
+    assert crv.balanceOf(user) == 0
+    assert vesting.balanceOf(user) == 0
